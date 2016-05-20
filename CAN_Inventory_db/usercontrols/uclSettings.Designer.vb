8c66f658f33f63089fab050b9679cd57bc419922
Philip Fibranz <pmfibran@svsu.edu>
2016-05-20 13:39:45 -0400

    Started adding location manager section to settings. Can load facility data and location data is still being tweeked. Want to pull out sections of initial load sub in to seperate subs that can be called throughout the use of the panel to aboid refreshing the entire panel at once when updating data to some of the components.

--- CAN_Inventory_db/usercontrols/uclSettings.Designer.vb
+++ CAN_Inventory_db/usercontrols/uclSettings.Designer.vb
@@ -27,7 +27,7 @@ Partial Class uclSettings
         Me.cbxCat4Sub = New System.Windows.Forms.ComboBox()
         Me.lnkRmvSubCat = New System.Windows.Forms.LinkLabel()
         Me.lbxSubCat = New System.Windows.Forms.ListBox()
-        Me.LinkLabel5 = New System.Windows.Forms.LinkLabel()
+        Me.lnkAddSubCat = New System.Windows.Forms.LinkLabel()
         Me.txtNewSubCat = New System.Windows.Forms.TextBox()
         Me.lbl_SSettings = New System.Windows.Forms.Label()
         Me.gbxItemCat = New System.Windows.Forms.GroupBox()
@@ -35,8 +35,32 @@ Partial Class uclSettings
         Me.lbxCategories = New System.Windows.Forms.ListBox()
         Me.lnkAddCat = New System.Windows.Forms.LinkLabel()
         Me.txtNewCat = New System.Windows.Forms.TextBox()
+        Me.gbxFacilities = New System.Windows.Forms.GroupBox()
+        Me.txtAddFacility = New System.Windows.Forms.TextBox()
+        Me.lbxFacilities = New System.Windows.Forms.ListBox()
+        Me.lnkRmvFacility = New System.Windows.Forms.LinkLabel()
+        Me.lnkAddFacility = New System.Windows.Forms.LinkLabel()
+        Me.gbxLocations = New System.Windows.Forms.GroupBox()
+        Me.lnkAddLocation = New System.Windows.Forms.LinkLabel()
+        Me.lnkRmvLocation = New System.Windows.Forms.LinkLabel()
+        Me.lbxLocations = New System.Windows.Forms.ListBox()
+        Me.txtAddLocation = New System.Windows.Forms.TextBox()
+        Me.gbxBins = New System.Windows.Forms.GroupBox()
+        Me.lnkAddBin = New System.Windows.Forms.LinkLabel()
+        Me.lnkRmvBin = New System.Windows.Forms.LinkLabel()
+        Me.lbxBins = New System.Windows.Forms.ListBox()
+        Me.txtAddBin = New System.Windows.Forms.TextBox()
+        Me.lblFacilityL = New System.Windows.Forms.Label()
+        Me.cbxFacilityL = New System.Windows.Forms.ComboBox()
+        Me.cbxFacilityB = New System.Windows.Forms.ComboBox()
+        Me.lblFacilityB = New System.Windows.Forms.Label()
+        Me.cbxLocationB = New System.Windows.Forms.ComboBox()
+        Me.lblLocationB = New System.Windows.Forms.Label()
         Me.gbxItemSubCat.SuspendLayout()
         Me.gbxItemCat.SuspendLayout()
+        Me.gbxFacilities.SuspendLayout()
+        Me.gbxLocations.SuspendLayout()
+        Me.gbxBins.SuspendLayout()
         Me.SuspendLayout()
         '
         'gbxItemSubCat
@@ -45,7 +69,7 @@ Partial Class uclSettings
         Me.gbxItemSubCat.Controls.Add(Me.cbxCat4Sub)
         Me.gbxItemSubCat.Controls.Add(Me.lnkRmvSubCat)
         Me.gbxItemSubCat.Controls.Add(Me.lbxSubCat)
-        Me.gbxItemSubCat.Controls.Add(Me.LinkLabel5)
+        Me.gbxItemSubCat.Controls.Add(Me.lnkAddSubCat)
         Me.gbxItemSubCat.Controls.Add(Me.txtNewSubCat)
         Me.gbxItemSubCat.Location = New System.Drawing.Point(12, 300)
         Me.gbxItemSubCat.Name = "gbxItemSubCat"
@@ -74,6 +98,7 @@ Partial Class uclSettings
         'lnkRmvSubCat
         '
         Me.lnkRmvSubCat.AutoSize = True
+        Me.lnkRmvSubCat.Enabled = False
         Me.lnkRmvSubCat.Location = New System.Drawing.Point(3, 198)
         Me.lnkRmvSubCat.Name = "lnkRmvSubCat"
         Me.lnkRmvSubCat.Size = New System.Drawing.Size(92, 13)
@@ -83,25 +108,28 @@ Partial Class uclSettings
         '
         'lbxSubCat
         '
+        Me.lbxSubCat.Enabled = False
         Me.lbxSubCat.FormattingEnabled = True
         Me.lbxSubCat.Location = New System.Drawing.Point(7, 87)
         Me.lbxSubCat.Name = "lbxSubCat"
-        Me.lbxSubCat.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lbxSubCat.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
         Me.lbxSubCat.Size = New System.Drawing.Size(173, 108)
         Me.lbxSubCat.TabIndex = 2
         '
-        'LinkLabel5
+        'lnkAddSubCat
         '
-        Me.LinkLabel5.AutoSize = True
-        Me.LinkLabel5.Location = New System.Drawing.Point(153, 63)
-        Me.LinkLabel5.Name = "LinkLabel5"
-        Me.LinkLabel5.Size = New System.Drawing.Size(26, 13)
-        Me.LinkLabel5.TabIndex = 1
-        Me.LinkLabel5.TabStop = True
-        Me.LinkLabel5.Text = "Add"
+        Me.lnkAddSubCat.AutoSize = True
+        Me.lnkAddSubCat.Enabled = False
+        Me.lnkAddSubCat.Location = New System.Drawing.Point(153, 63)
+        Me.lnkAddSubCat.Name = "lnkAddSubCat"
+        Me.lnkAddSubCat.Size = New System.Drawing.Size(26, 13)
+        Me.lnkAddSubCat.TabIndex = 1
+        Me.lnkAddSubCat.TabStop = True
+        Me.lnkAddSubCat.Text = "Add"
         '
         'txtNewSubCat
         '
+        Me.txtNewSubCat.Enabled = False
         Me.txtNewSubCat.Location = New System.Drawing.Point(7, 60)
         Me.txtNewSubCat.Name = "txtNewSubCat"
         Me.txtNewSubCat.Size = New System.Drawing.Size(141, 20)
@@ -145,7 +173,7 @@ Partial Class uclSettings
         Me.lbxCategories.FormattingEnabled = True
         Me.lbxCategories.Location = New System.Drawing.Point(6, 45)
         Me.lbxCategories.Name = "lbxCategories"
-        Me.lbxCategories.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.lbxCategories.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
         Me.lbxCategories.Size = New System.Drawing.Size(173, 108)
         Me.lbxCategories.TabIndex = 2
         '
@@ -166,10 +194,226 @@ Partial Class uclSettings
         Me.txtNewCat.Size = New System.Drawing.Size(141, 20)
         Me.txtNewCat.TabIndex = 0
         '
+        'gbxFacilities
+        '
+        Me.gbxFacilities.Controls.Add(Me.lnkAddFacility)
+        Me.gbxFacilities.Controls.Add(Me.lnkRmvFacility)
+        Me.gbxFacilities.Controls.Add(Me.lbxFacilities)
+        Me.gbxFacilities.Controls.Add(Me.txtAddFacility)
+        Me.gbxFacilities.Location = New System.Drawing.Point(241, 84)
+        Me.gbxFacilities.Name = "gbxFacilities"
+        Me.gbxFacilities.Size = New System.Drawing.Size(200, 187)
+        Me.gbxFacilities.TabIndex = 6
+        Me.gbxFacilities.TabStop = False
+        Me.gbxFacilities.Text = "Facilities"
+        '
+        'txtAddFacility
+        '
+        Me.txtAddFacility.Location = New System.Drawing.Point(6, 19)
+        Me.txtAddFacility.Name = "txtAddFacility"
+        Me.txtAddFacility.Size = New System.Drawing.Size(129, 20)
+        Me.txtAddFacility.TabIndex = 0
+        '
+        'lbxFacilities
+        '
+        Me.lbxFacilities.FormattingEnabled = True
+        Me.lbxFacilities.Location = New System.Drawing.Point(7, 46)
+        Me.lbxFacilities.Name = "lbxFacilities"
+        Me.lbxFacilities.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lbxFacilities.Size = New System.Drawing.Size(173, 108)
+        Me.lbxFacilities.TabIndex = 1
+        '
+        'lnkRmvFacility
+        '
+        Me.lnkRmvFacility.AutoSize = True
+        Me.lnkRmvFacility.Location = New System.Drawing.Point(4, 156)
+        Me.lnkRmvFacility.Name = "lnkRmvFacility"
+        Me.lnkRmvFacility.Size = New System.Drawing.Size(92, 13)
+        Me.lnkRmvFacility.TabIndex = 2
+        Me.lnkRmvFacility.TabStop = True
+        Me.lnkRmvFacility.Text = "Remove Selected"
+        '
+        'lnkAddFacility
+        '
+        Me.lnkAddFacility.AutoSize = True
+        Me.lnkAddFacility.Location = New System.Drawing.Point(141, 22)
+        Me.lnkAddFacility.Name = "lnkAddFacility"
+        Me.lnkAddFacility.Size = New System.Drawing.Size(26, 13)
+        Me.lnkAddFacility.TabIndex = 3
+        Me.lnkAddFacility.TabStop = True
+        Me.lnkAddFacility.Text = "Add"
+        '
+        'gbxLocations
+        '
+        Me.gbxLocations.Controls.Add(Me.cbxFacilityL)
+        Me.gbxLocations.Controls.Add(Me.lblFacilityL)
+        Me.gbxLocations.Controls.Add(Me.lnkAddLocation)
+        Me.gbxLocations.Controls.Add(Me.lnkRmvLocation)
+        Me.gbxLocations.Controls.Add(Me.lbxLocations)
+        Me.gbxLocations.Controls.Add(Me.txtAddLocation)
+        Me.gbxLocations.Location = New System.Drawing.Point(241, 300)
+        Me.gbxLocations.Name = "gbxLocations"
+        Me.gbxLocations.Size = New System.Drawing.Size(200, 218)
+        Me.gbxLocations.TabIndex = 7
+        Me.gbxLocations.TabStop = False
+        Me.gbxLocations.Text = "Locations"
+        '
+        'lnkAddLocation
+        '
+        Me.lnkAddLocation.AutoSize = True
+        Me.lnkAddLocation.Enabled = False
+        Me.lnkAddLocation.Location = New System.Drawing.Point(141, 63)
+        Me.lnkAddLocation.Name = "lnkAddLocation"
+        Me.lnkAddLocation.Size = New System.Drawing.Size(26, 13)
+        Me.lnkAddLocation.TabIndex = 3
+        Me.lnkAddLocation.TabStop = True
+        Me.lnkAddLocation.Text = "Add"
+        '
+        'lnkRmvLocation
+        '
+        Me.lnkRmvLocation.AutoSize = True
+        Me.lnkRmvLocation.Enabled = False
+        Me.lnkRmvLocation.Location = New System.Drawing.Point(4, 197)
+        Me.lnkRmvLocation.Name = "lnkRmvLocation"
+        Me.lnkRmvLocation.Size = New System.Drawing.Size(92, 13)
+        Me.lnkRmvLocation.TabIndex = 2
+        Me.lnkRmvLocation.TabStop = True
+        Me.lnkRmvLocation.Text = "Remove Selected"
+        '
+        'lbxLocations
+        '
+        Me.lbxLocations.Enabled = False
+        Me.lbxLocations.FormattingEnabled = True
+        Me.lbxLocations.Location = New System.Drawing.Point(7, 87)
+        Me.lbxLocations.Name = "lbxLocations"
+        Me.lbxLocations.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lbxLocations.Size = New System.Drawing.Size(173, 108)
+        Me.lbxLocations.TabIndex = 1
+        '
+        'txtAddLocation
+        '
+        Me.txtAddLocation.Enabled = False
+        Me.txtAddLocation.Location = New System.Drawing.Point(6, 60)
+        Me.txtAddLocation.Name = "txtAddLocation"
+        Me.txtAddLocation.Size = New System.Drawing.Size(129, 20)
+        Me.txtAddLocation.TabIndex = 0
+        '
+        'gbxBins
+        '
+        Me.gbxBins.Controls.Add(Me.cbxLocationB)
+        Me.gbxBins.Controls.Add(Me.lblLocationB)
+        Me.gbxBins.Controls.Add(Me.cbxFacilityB)
+        Me.gbxBins.Controls.Add(Me.lblFacilityB)
+        Me.gbxBins.Controls.Add(Me.lnkAddBin)
+        Me.gbxBins.Controls.Add(Me.lnkRmvBin)
+        Me.gbxBins.Controls.Add(Me.lbxBins)
+        Me.gbxBins.Controls.Add(Me.txtAddBin)
+        Me.gbxBins.Location = New System.Drawing.Point(447, 84)
+        Me.gbxBins.Name = "gbxBins"
+        Me.gbxBins.Size = New System.Drawing.Size(200, 434)
+        Me.gbxBins.TabIndex = 7
+        Me.gbxBins.TabStop = False
+        Me.gbxBins.Text = "Bins"
+        '
+        'lnkAddBin
+        '
+        Me.lnkAddBin.AutoSize = True
+        Me.lnkAddBin.Enabled = False
+        Me.lnkAddBin.Location = New System.Drawing.Point(141, 98)
+        Me.lnkAddBin.Name = "lnkAddBin"
+        Me.lnkAddBin.Size = New System.Drawing.Size(26, 13)
+        Me.lnkAddBin.TabIndex = 3
+        Me.lnkAddBin.TabStop = True
+        Me.lnkAddBin.Text = "Add"
+        '
+        'lnkRmvBin
+        '
+        Me.lnkRmvBin.AutoSize = True
+        Me.lnkRmvBin.Enabled = False
+        Me.lnkRmvBin.Location = New System.Drawing.Point(3, 414)
+        Me.lnkRmvBin.Name = "lnkRmvBin"
+        Me.lnkRmvBin.Size = New System.Drawing.Size(92, 13)
+        Me.lnkRmvBin.TabIndex = 2
+        Me.lnkRmvBin.TabStop = True
+        Me.lnkRmvBin.Text = "Remove Selected"
+        '
+        'lbxBins
+        '
+        Me.lbxBins.Enabled = False
+        Me.lbxBins.FormattingEnabled = True
+        Me.lbxBins.Location = New System.Drawing.Point(6, 121)
+        Me.lbxBins.Name = "lbxBins"
+        Me.lbxBins.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lbxBins.Size = New System.Drawing.Size(173, 290)
+        Me.lbxBins.TabIndex = 1
+        '
+        'txtAddBin
+        '
+        Me.txtAddBin.Enabled = False
+        Me.txtAddBin.Location = New System.Drawing.Point(6, 95)
+        Me.txtAddBin.Name = "txtAddBin"
+        Me.txtAddBin.Size = New System.Drawing.Size(129, 20)
+        Me.txtAddBin.TabIndex = 0
+        '
+        'lblFacilityL
+        '
+        Me.lblFacilityL.AutoSize = True
+        Me.lblFacilityL.Location = New System.Drawing.Point(4, 26)
+        Me.lblFacilityL.Name = "lblFacilityL"
+        Me.lblFacilityL.Size = New System.Drawing.Size(42, 13)
+        Me.lblFacilityL.TabIndex = 4
+        Me.lblFacilityL.Text = "Facility:"
+        '
+        'cbxFacilityL
+        '
+        Me.cbxFacilityL.FormattingEnabled = True
+        Me.cbxFacilityL.Location = New System.Drawing.Point(52, 23)
+        Me.cbxFacilityL.Name = "cbxFacilityL"
+        Me.cbxFacilityL.Size = New System.Drawing.Size(128, 21)
+        Me.cbxFacilityL.TabIndex = 5
+        '
+        'cbxFacilityB
+        '
+        Me.cbxFacilityB.FormattingEnabled = True
+        Me.cbxFacilityB.Location = New System.Drawing.Point(51, 19)
+        Me.cbxFacilityB.Name = "cbxFacilityB"
+        Me.cbxFacilityB.Size = New System.Drawing.Size(128, 21)
+        Me.cbxFacilityB.TabIndex = 7
+        '
+        'lblFacilityB
+        '
+        Me.lblFacilityB.AutoSize = True
+        Me.lblFacilityB.Location = New System.Drawing.Point(3, 22)
+        Me.lblFacilityB.Name = "lblFacilityB"
+        Me.lblFacilityB.Size = New System.Drawing.Size(42, 13)
+        Me.lblFacilityB.TabIndex = 6
+        Me.lblFacilityB.Text = "Facility:"
+        '
+        'cbxLocationB
+        '
+        Me.cbxLocationB.Enabled = False
+        Me.cbxLocationB.FormattingEnabled = True
+        Me.cbxLocationB.Location = New System.Drawing.Point(60, 52)
+        Me.cbxLocationB.Name = "cbxLocationB"
+        Me.cbxLocationB.Size = New System.Drawing.Size(119, 21)
+        Me.cbxLocationB.TabIndex = 9
+        '
+        'lblLocationB
+        '
+        Me.lblLocationB.AutoSize = True
+        Me.lblLocationB.Location = New System.Drawing.Point(3, 55)
+        Me.lblLocationB.Name = "lblLocationB"
+        Me.lblLocationB.Size = New System.Drawing.Size(51, 13)
+        Me.lblLocationB.TabIndex = 8
+        Me.lblLocationB.Text = "Location:"
+        '
         'uclSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.gbxBins)
+        Me.Controls.Add(Me.gbxLocations)
+        Me.Controls.Add(Me.gbxFacilities)
         Me.Controls.Add(Me.gbxItemSubCat)
         Me.Controls.Add(Me.lbl_SSettings)
         Me.Controls.Add(Me.gbxItemCat)
@@ -179,6 +423,12 @@ Partial Class uclSettings
         Me.gbxItemSubCat.PerformLayout()
         Me.gbxItemCat.ResumeLayout(False)
         Me.gbxItemCat.PerformLayout()
+        Me.gbxFacilities.ResumeLayout(False)
+        Me.gbxFacilities.PerformLayout()
+        Me.gbxLocations.ResumeLayout(False)
+        Me.gbxLocations.PerformLayout()
+        Me.gbxBins.ResumeLayout(False)
+        Me.gbxBins.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -189,7 +439,7 @@ Partial Class uclSettings
     Friend WithEvents cbxCat4Sub As ComboBox
     Friend WithEvents lnkRmvSubCat As LinkLabel
     Friend WithEvents lbxSubCat As ListBox
-    Friend WithEvents LinkLabel5 As LinkLabel
+    Friend WithEvents lnkAddSubCat As LinkLabel
     Friend WithEvents txtNewSubCat As TextBox
     Friend WithEvents lbl_SSettings As Label
     Friend WithEvents gbxItemCat As GroupBox
@@ -197,4 +447,25 @@ Partial Class uclSettings
     Friend WithEvents lbxCategories As ListBox
     Friend WithEvents lnkAddCat As LinkLabel
     Friend WithEvents txtNewCat As TextBox
+    Friend WithEvents gbxFacilities As GroupBox
+    Friend WithEvents lnkAddFacility As LinkLabel
+    Friend WithEvents lnkRmvFacility As LinkLabel
+    Friend WithEvents lbxFacilities As ListBox
+    Friend WithEvents txtAddFacility As TextBox
+    Friend WithEvents gbxLocations As GroupBox
+    Friend WithEvents cbxFacilityL As ComboBox
+    Friend WithEvents lblFacilityL As Label
+    Friend WithEvents lnkAddLocation As LinkLabel
+    Friend WithEvents lnkRmvLocation As LinkLabel
+    Friend WithEvents lbxLocations As ListBox
+    Friend WithEvents txtAddLocation As TextBox
+    Friend WithEvents gbxBins As GroupBox
+    Friend WithEvents cbxLocationB As ComboBox
+    Friend WithEvents lblLocationB As Label
+    Friend WithEvents cbxFacilityB As ComboBox
+    Friend WithEvents lblFacilityB As Label
+    Friend WithEvents lnkAddBin As LinkLabel
+    Friend WithEvents lnkRmvBin As LinkLabel
+    Friend WithEvents lbxBins As ListBox
+    Friend WithEvents txtAddBin As TextBox
 End Class
--- CAN_Inventory_db/usercontrols/uclSettings.vb
+++ CAN_Inventory_db/usercontrols/uclSettings.vb
@@ -32,7 +32,7 @@
 
     End Sub
 
-    Private Sub LinkLabel5_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel5.LinkClicked
+    Private Sub LinkLabel5_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkAddSubCat.LinkClicked
         Dim data(1) As String
         Dim subCatID As Integer
 
@@ -59,59 +59,20 @@
     End Sub
 
     Private Sub cbxCat4Sub_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxCat4Sub.SelectedIndexChanged
-
-        SubCatListLoad()
-
-    End Sub
-
-    'Settings Panel Load Sub
-    Sub SettingsControlLoad()
-        Dim dsCat As New DataSet()
-        Dim dsCat2 As New DataSet()
-        Dim query As String
-
-        ready = False
-
-
-        query = frmMain.dbAccess.QueryBuilder("categories", "*", "(((categories.active)=True))")
-        dsCat = frmMain.dbAccess.DataGet(query)
-
-        lbxCategories.DataSource = dsCat.Tables(0)
-        lbxCategories.ValueMember = "id"
-        lbxCategories.DisplayMember = "category"
-        lbxCategories.SelectedIndex = -1
-
-        dsCat2 = dsCat.Copy()
-
-        cbxCat4Sub.DataSource = dsCat2.Tables(0)
-        cbxCat4Sub.ValueMember = "id"
-        cbxCat4Sub.DisplayMember = "category"
-        cbxCat4Sub.SelectedIndex = -1
-
-        ready = True
-    End Sub
-
-    Sub SubCatListLoad()
-        Dim dsSubCat As New DataSet()
-        Dim query As String
-        Dim sqlCriteria As String
         If ready = True Then
-            Try
-                sqlCriteria = "(((sub_categories.category_id)=" + cbxCat4Sub.SelectedValue.ToString() +
-                            ") AND ((sub_categories.active)=True))"
+            SubCatListLoad()
 
-                query = frmMain.dbAccess.QueryBuilder("sub_categories", "*", sqlCriteria)
-                dsSubCat = frmMain.dbAccess.DataGet(query)
-
-                lbxSubCat.DataSource = dsSubCat.Tables(0)
-                lbxSubCat.ValueMember = "id"
-                lbxSubCat.DisplayMember = "sub_category"
-                'lbxSubCat.SelectedIndex = -1
-            Catch ex As Exception
-                frmMain.lblStatus.Text = ex.Message()
-            End Try
+            txtNewSubCat.Enabled = True
+            lnkAddSubCat.Enabled = True
+            lbxSubCat.Enabled = True
+            lnkRmvSubCat.Enabled = True
         End If
+
     End Sub
+
+
+
+
 
     Private Sub lnkRmvCat_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkRmvCat.LinkClicked
         Dim where(0) As String
@@ -147,4 +108,139 @@
 
         SubCatListLoad()
     End Sub
+
+    Private Sub cbxFacilityL_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxFacilityL.SelectedIndexChanged
+        If ready = True Then
+            LocationListLoad(lbxLocations, cbxFacilityL)
+
+            txtAddLocation.Enabled = True
+            lnkAddLocation.Enabled = True
+            lbxLocations.Enabled = True
+            lnkRmvLocation.Enabled = True
+        End If
+    End Sub
+
+
+    Private Sub cbxFacilityB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxFacilityB.SelectedIndexChanged
+        If ready = True Then
+            LocationListLoad(cbxLocationB, cbxFacilityB)
+            cbxLocationB.SelectedIndex = -1
+            cbxLocationB.Enabled = True
+
+        End If
+    End Sub
+
+
+    'Settings Panel Load Sub
+    Sub SettingsControlLoad()
+
+        'Categories
+        CategoryListLoad(lbxCategories)
+        lbxCategories.SelectedIndex = -1
+
+        CategoryListLoad(cbxCat4Sub)
+        cbxCat4Sub.SelectedIndex = -1
+
+        'Facilities
+
+        FacilityListLoad(lbxFacilities)
+        lbxFacilities.SelectedIndex = -1
+
+        FacilityListLoad(cbxFacilityL)
+        cbxFacilityL.SelectedIndex = -1
+
+        FacilityListLoad(cbxFacilityB)
+        cbxFacilityB.SelectedIndex = -1
+
+
+        ready = True
+    End Sub
+
+    'Load Category Listbox
+    Sub CategoryListLoad(ob As Object)
+        Dim dsCat As New DataSet()
+        Dim query As String
+
+        ready = False
+
+        'Categories
+        query = frmMain.dbAccess.QueryBuilder("categories", "*", "(((categories.active)=True))")
+        dsCat = frmMain.dbAccess.DataGet(query)
+
+        ob.DataSource = dsCat.Tables(0)
+        ob.ValueMember = "id"
+        ob.DisplayMember = "category"
+
+    End Sub
+
+
+    'Load Sub Category Listbox
+    Sub SubCatListLoad()
+        Dim dsSubCat As New DataSet()
+        Dim query As String
+        Dim sqlCriteria As String
+
+        Try
+            sqlCriteria = "(((sub_categories.category_id)=" + cbxCat4Sub.SelectedValue.ToString() +
+                            ") AND ((sub_categories.active)=True))"
+
+            query = frmMain.dbAccess.QueryBuilder("sub_categories", "*", sqlCriteria)
+            dsSubCat = frmMain.dbAccess.DataGet(query)
+
+            lbxSubCat.DataSource = dsSubCat.Tables(0)
+            lbxSubCat.ValueMember = "id"
+            lbxSubCat.DisplayMember = "sub_category"
+            'lbxSubCat.SelectedIndex = -1
+        Catch ex As Exception
+            frmMain.lblStatus.Text = ex.Message()
+        End Try
+    End Sub
+
+    'Loads Facility Listbox  ****** (So far just copy of location use to replace larger initial settings control load.
+    Sub FacilityListLoad(ob As Object)
+        Dim dsFacility As New DataSet()
+        Dim query As String
+
+        Try
+
+            query = frmMain.dbAccess.QueryBuilder("facilities", "*", "(((facilities.active)=True))")
+            dsFacility = frmMain.dbAccess.DataGet(query)
+
+            ob.DataSource = dsFacility.Tables(0)
+            ob.ValueMember = "id"
+            ob.DisplayMember = "facility"
+            'ob.SelectedIndex = -1
+        Catch ex As Exception
+            frmMain.lblStatus.Text = ex.Message()
+        End Try
+    End Sub
+
+    'Loads Location  *************  Add parameters to handle different objects to input dataset
+    Sub LocationListLoad(ob As Object, Optional from As ComboBox = Nothing)
+        Dim dsLocation As New DataSet()
+        Dim query As String
+        Dim sqlCriteria As String
+
+        Try
+            If from Is Nothing Then
+                sqlCriteria = "((locations.active)=True))"
+            Else
+                sqlCriteria = "(((locations.facility_id)=" + from.SelectedValue.ToString() +
+                            ") AND ((locations.active)=True))"
+            End If
+
+
+            query = frmMain.dbAccess.QueryBuilder("locations", "*", sqlCriteria)
+            dsLocation = frmMain.dbAccess.DataGet(query)
+
+            ob.DataSource = dsLocation.Tables(0)
+            ob.ValueMember = "id"
+            ob.DisplayMember = "location"
+            'lbxLocations.SelectedIndex = -1
+        Catch ex As Exception
+            frmMain.lblStatus.Text = ex.Message()
+        End Try
+    End Sub
+
+
 End Class
